68983d3fa340892d8b33f74b33750ebe88714941
techn0z <minhtrietamoled@gmail.com>
2025-12-31 18:42:52 +0700

    update

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ def main():
     model.train(
         data="yolo_dataset/data.yaml",
         epochs = 100 + 1,
-        imgsz = 1280,
+        imgsz = 960,
         batch = 8,
         nbs = 64,           
         pretrained = True,
