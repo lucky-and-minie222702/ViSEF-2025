ff4297f32c8a616843da6b6fdf4bba7f633cf203
techn0z <minhtrietamoled@gmail.com>
2026-01-01 21:34:59 +0700

    update

--- train.py
+++ train.py
@@ -6,20 +6,19 @@ config = {
 }
 
 def main():
-    model = YOLO("yolo11m.pt")  
+    model = YOLO("yolo11s.pt")  
 
     # Train
     model.train(
-        data="yolo_dataset/data.yaml",
+        data = "yolo_dataset/data.yaml",
         epochs = 300 + 1,
-        imgsz = 1280,
-        batch = 16,
+        imgsz = 640,
+        batch = 64,
         nbs = 64,           
         pretrained = True,
         
         device = [0, 1],
-        cls = 2.0,
-        box = 5.0,
+        box = 10.0,
 
         optimizer = "Adam",
         lr0 = 0.001,
@@ -27,28 +26,29 @@ def main():
         weight_decay = 0.0005,
         warmup_epochs = 3,
 
-        # multi_scale = True,
+        multi_scale = True,
         hsv_h = 0.015,
         hsv_s = 0.8,
         hsv_v = 0.5,
         degrees = 3.6, 
         translate = 0.05,   
-        scale = 0.75,
+        scale = 0.5,
         perspective = 0.00001,
         flipud = 0.0, # no vertical flip
         fliplr = 0.5, # 50% horizontal flip
-        mosaic = 0.5,
+        mosaic = 0.0,
         copy_paste = 0.5,
-        mixup = 0.5,
+        mosaic = 0.2,
+        close_mosaic = 10,
 
         # Val
         val = True,
         save = True,
         save_period = 30,
-        project = "yolo11m_new/polyp_yolo",
+        project = "yolo11s/polyp_yolo",
         name = "yolo_det",
 
-        patience = 20,
+        patience = 1_000_000,
         deterministic = True,
         seed = 27022009,
         
