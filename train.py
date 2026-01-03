43ff245599a8f958d7b44b7d3e63a28140293a3e
techn0z <minhtrietamoled@gmail.com>
2026-01-03 16:16:15 +0700

    update

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ def main():
         box = 10.0,
 
         optimizer = "Adam",
-        lr0 = 0.001,
+        lr0 = 0.0009,
         lrf = 0.01,
         weight_decay = 0.0005,
         warmup_epochs = 3,
