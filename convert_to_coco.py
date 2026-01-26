8e7056e15b8b7e73100cb10f3de06b4fff40b819
techn0z <minhtrietamoled@gmail.com>
2026-01-26 22:06:37 +0700

    update

--- convert_to_coco.py
+++ convert_to_coco.py
@@ -5,9 +5,8 @@ dataset_dir = "yolo_dataset"
 coco_output_dir = "coco_dataset"
 
 for split in ["train", "val", "test"]:
-    split_path = os.path.join(dataset_dir, split)
-    images_path = os.path.join(split_path, "images")
-    labels_path = os.path.join(split_path, "labels")
+    images_path = os.path.join(dataset_dir, "images", split)
+    labels_path = os.path.join(dataset_dir, "labels", split)
     
     ds = sv.DetectionDataset.from_yolo(
         images_directory_path=images_path,
