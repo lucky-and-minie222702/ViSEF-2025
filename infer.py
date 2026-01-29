6bff18617e1ac300a0bf4addca764d339389abf8
techn0z <minhtrietamoled@gmail.com>
2026-01-29 07:49:26 +0700

    update

--- infer.py
+++ infer.py
@@ -11,7 +11,7 @@ import torch
 import os
 import math
 
-MODEL_PATH = "runs/detect/new_yolo26s/polyp_yolo/yolo_det/weights/last.pt"
+MODEL_PATH = "runs/detect/yolo26s/polyp_yolo/yolo_det/weights/last.pt"
 
 IMG_SIZE = 640,
 CONF_THRES = 0.5
